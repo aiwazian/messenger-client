d09c4c3c477637adb9000b9c08a92671ff4b9763
karenaiwazian <karenaiwazian@outlook.com>
2026-06-19 17:12:17 +0300

    Update app version code from 35 to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.aiwazian.messenger"
         minSdk = 30
         targetSdk = 37
-        versionCode = 35
-        versionName = "1.13.2"
+        versionCode = 36
+        versionName = "1.13.3"
     }
     
     buildTypes {
