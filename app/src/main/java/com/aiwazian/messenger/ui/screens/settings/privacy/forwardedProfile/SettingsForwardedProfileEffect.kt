51460f83191d9535679bcd422e9fc1ea48a17cf2
Karen Aiwazian <karenaiwazian@outlook.com>
2026-07-28 13:42:16 +0300

    feat(privacy): экран настройки пересылки сообщений — effect

---
+++ app/src/main/java/com/aiwazian/messenger/ui/screens/settings/privacy/forwardedProfile/SettingsForwardedProfileEffect.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2026. Aiwazian.
+ */
+
+package com.aiwazian.messenger.ui.screens.settings.privacy.forwardedProfile
+
+sealed interface SettingsForwardedProfileEffect {
+    object Back : SettingsForwardedProfileEffect
+}
