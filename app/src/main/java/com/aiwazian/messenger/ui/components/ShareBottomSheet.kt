7f2ec7de875155c84c9252f225f931cacd989f3c
karenaiwazian <karenaiwazian@outlook.com>
2026-08-12 12:43:55 +0300

    Fix

--- app/src/main/java/com/aiwazian/messenger/ui/components/ShareBottomSheet.kt
+++ app/src/main/java/com/aiwazian/messenger/ui/components/ShareBottomSheet.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
@@ -63,7 +62,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -150,7 +148,8 @@ fun ShareBottomSheet(
                         unfocusedIndicatorColor = Color.Transparent,
                     ),
                     modifier = Modifier
-                        .padding(start = 10.dp, top = 10.dp)
+                        .fillMaxWidth()
+                        .padding(10.dp)
                         .statusBarsPadding(),
                     interactionSource = interactionSource
                 )
@@ -177,23 +176,6 @@ fun ShareBottomSheet(
                     }
                 }
             }
-            
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth()
-                    .height(40.dp)
-                    .imePadding()
-                    .offset { IntOffset(x = 0, y = -sheetState.requireOffset().toInt()) }
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                MaterialTheme.colorScheme.surface.copy(0.8f),
-                            )
-                        )
-                    )
-            )
             
             androidx.compose.animation.AnimatedVisibility(
                 visible = hasSelected,
