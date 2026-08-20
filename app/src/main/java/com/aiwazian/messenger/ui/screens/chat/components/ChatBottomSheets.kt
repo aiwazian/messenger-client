664e467d36644d4ca74b44e47d59dac996296f73
Karen Aiwazian <karenaiwazian@outlook.com>
2026-08-20 18:42:18 +0300

    New: удалён старый AttachmentBottomSheet

--- app/src/main/java/com/aiwazian/messenger/ui/screens/chat/components/ChatBottomSheets.kt
+++ app/src/main/java/com/aiwazian/messenger/ui/screens/chat/components/ChatBottomSheets.kt
@@ -7,14 +7,10 @@ package com.aiwazian.messenger.ui.screens.chat.components
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,11 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Mic
-import androidx.compose.material.icons.rounded.Photo
-import androidx.compose.material.icons.rounded.Storage
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularWavyProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -40,7 +32,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
@@ -51,107 +42,6 @@ import androidx.compose.ui.unit.sp
 import com.aiwazian.messenger.R
 import com.aiwazian.messenger.enums.ChatType
 import com.aiwazian.messenger.ui.app.AppBottomSheet
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AttachmentBottomSheet(
-    onDismissRequest: () -> Unit,
-    onFileSystemClick: () -> Unit,
-    onFileSelected: (List<Uri>) -> Unit
-) {
-    val sheetState = rememberBottomSheetState(initialValue = SheetValue.Hidden)
-    
-    AppBottomSheet(sheetState = sheetState, onDismissRequest = onDismissRequest) {
-        Card(
-            onClick = onFileSystemClick,
-            modifier = Modifier.fillMaxWidth(),
-            shape = CircleShape,
-            colors = CardDefaults.cardColors(containerColor = Color.Transparent)
-        ) {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.primaryContainer)
-                ) {
-                    Icon(
-                        modifier = Modifier.padding(10.dp),
-                        imageVector = Icons.Rounded.Storage,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer
-                    )
-                }
-                
-                Column {
-                    Text(
-                        text = stringResource(R.string.internal_storage),
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                    Text(
-                        text = stringResource(R.string.file_system_search),
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        fontSize = 12.sp,
-                        lineHeight = 12.sp
-                    )
-                }
-            }
-        }
-        
-        val d = rememberLauncherForActivityResult(
-            ActivityResultContracts.PickMultipleVisualMedia(
-                10
-            )
-        ) { uris ->
-            if (uris.isNotEmpty()) {
-                onFileSelected(uris)
-            }
-        }
-        Card(
-            onClick = {
-                d.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
-            },
-            modifier = Modifier.fillMaxWidth(),
-            shape = CircleShape,
-            colors = CardDefaults.cardColors(containerColor = Color.Transparent)
-        ) {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Box(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.secondaryContainer)
-                ) {
-                    Icon(
-                        modifier = Modifier.padding(10.dp),
-                        imageVector = Icons.Rounded.Photo,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer
-                    )
-                }
-                
-                Column {
-                    Text(
-                        text = stringResource(R.string.gallery),
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                    Text(
-                        text = stringResource(R.string.to_send_images_without_compression),
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        fontSize = 12.sp,
-                        lineHeight = 12.sp
-                    )
-                }
-            }
-        }
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
