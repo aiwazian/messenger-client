96bb26e54cec9180647647af72bf7e78c39e24af
karenaiwazian <karenaiwazian@outlook.com>
2026-09-11 20:22:24 +0300

    Fix

--- app/src/main/java/com/aiwazian/messenger/utils/VibrationPattern.kt
+++ app/src/main/java/com/aiwazian/messenger/utils/VibrationPattern.kt
@@ -5,7 +5,6 @@
 package com.aiwazian.messenger.utils
 
 object VibrationPattern {
-    val Notification = longArrayOf(0, 300, 200, 300)
     val Error = longArrayOf(0, 50, 50, 50)
     val TactileResponse = longArrayOf(0, 50)
 }
