7416f515d86b26c7b32f8731d4d948575165b5bd
Karen Aiwazian <karenaiwazian@outlook.com>
2026-09-12 21:49:03 +0300

    Migrate emoji pack editor to coil3

--- app/src/main/java/com/aiwazian/messenger/ui/screens/settings/emoji/created/EmojiPackEditorScreen.kt
+++ app/src/main/java/com/aiwazian/messenger/ui/screens/settings/emoji/created/EmojiPackEditorScreen.kt
@@ -85,8 +85,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.lerp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
 import com.aiwazian.messenger.R
 import com.aiwazian.messenger.ui.animations.expressiveScaleIn
 import com.aiwazian.messenger.ui.animations.expressiveScaleOut
