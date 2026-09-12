26de2a8e933d175d11b13a721a642c2f42df21bf
Karen Aiwazian <karenaiwazian@outlook.com>
2026-09-12 21:52:20 +0300

    Migrate emoji and sticker pack screens to coil3

--- app/src/main/java/com/aiwazian/messenger/ui/screens/settings/emoji/added/AddedEmojiPacksScreen.kt
+++ app/src/main/java/com/aiwazian/messenger/ui/screens/settings/emoji/added/AddedEmojiPacksScreen.kt
@@ -35,8 +35,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
 import com.aiwazian.messenger.R
 import com.aiwazian.messenger.ui.app.AppBottomSheet
 import com.aiwazian.messenger.ui.app.AppSnackbar
