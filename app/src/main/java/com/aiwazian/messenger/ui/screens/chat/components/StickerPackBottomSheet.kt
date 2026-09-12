3e78eb389e5d1a101508ea142476defdfdc10a03
Karen Aiwazian <karenaiwazian@outlook.com>
2026-09-12 22:06:38 +0300

    Migrate sticker pack bottom sheet to coil3

--- app/src/main/java/com/aiwazian/messenger/ui/screens/chat/components/StickerPackBottomSheet.kt
+++ app/src/main/java/com/aiwazian/messenger/ui/screens/chat/components/StickerPackBottomSheet.kt
@@ -64,8 +64,8 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.DialogWindowProvider
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
 import com.aiwazian.messenger.R
 import com.aiwazian.messenger.domain.Sticker
 import com.aiwazian.messenger.domain.StickerPack
